0220ee6eb07090ec3cdfdcda09bf02d90c458539
Ian Carvalho <iancarv@gmail.com>
2019-03-20 22:35:33 -0400

    bugfix

--- gan.py
+++ gan.py
@@ -234,7 +234,7 @@ class GAN(object):
                 # layer as a length one sequence
                 generated_images = self.generator.predict(
                     [noise, sampled_labels.reshape((-1, 1))], verbose=0)
-
+                print(image_batch.shape, generated_images.shape)
                 X = np.concatenate((image_batch, generated_images))
                 y = np.array([-1] * batch_size + [1] * batch_size)
                 aux_y = np.concatenate((label_batch, sampled_labels), axis=0)
