fa08fe9af82e6b0aff99f2e21b80b15e111660bc
Ian Carvalho <iancarv@gmail.com>
2019-03-20 23:10:14 -0400

    bugfix

--- gan.py
+++ gan.py
@@ -346,7 +346,7 @@ class GAN(object):
 if __name__ == '__main__':
 
     X_train, y_train, X_test, y_test = data.load_tmi_data()
-    config = Config(nb_epochs=2, channels=3, num_classes=2)
+    config = Config(nb_epochs=50, channels=3, num_classes=2)
     gan = GAN(config)
     train_history, test_history = gan.train(X_train, y_train, X_test, y_test)
     pickle.dump({'train': train_history, 'test': test_history},
