7716f1dcc7e07c286a7ac0e4a18d068812f6919d
Ian Carvalho <iancarv@gmail.com>
2019-03-20 21:39:09 -0400

    bugfix

--- gan.py
+++ gan.py
@@ -209,7 +209,7 @@ class GAN(object):
             epoch_gen_loss = []
             epoch_disc_loss = []
 
-            for index in range(1):
+            for index in range(nb_batches):
                 if len(epoch_gen_loss) + len(epoch_disc_loss) > 1:
                     progress_bar.update(index, values=[('disc_loss',np.mean(np.array(epoch_disc_loss),axis=0)[0]), ('gen_loss', np.mean(np.array(epoch_gen_loss),axis=0)[0])])
                 else:
