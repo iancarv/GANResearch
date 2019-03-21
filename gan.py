61f983c2d80ee23b1e9fae8c7eb581301dc83fb3
Ian Carvalho <iancarv@gmail.com>
2019-03-20 23:34:52 -0400

    bugfix

--- gan.py
+++ gan.py
@@ -331,6 +331,7 @@ class GAN(object):
             generated_images = self.generator.predict(
                 [noise, sampled_labels], verbose=0)
             print(generated_images.shape)
+            print(np.split(generated_images, config.num_classes).shape)
             # arrange them into a grid
             img = (np.concatenate([r
                                    for r in np.split(generated_images, config.num_classes)
