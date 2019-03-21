82972a5f30d581dfe3a8fd391a12202d16b5e581
Ian Carvalho <iancarv@gmail.com>
2019-03-21 12:49:30 -0400

    functions

--- utils.py
+++ utils.py
@@ -207,7 +207,7 @@ def test_model_from_results(path, results, thresh_nms=1, use_real=True):
       windows = r['windows']
       cell_patches = [cell[w[0]:w[2], w[1]:w[3]] for w in windows]
       
-      picks = nms(windows, np.amax(y_proba, axis=1), 0.1, thresh_nms)
+      picks = nms(windows, np.amax(y_proba, axis=1))
         
       y_scores = y_scores[picks]
       y_pred = y_pred[picks]
@@ -260,47 +260,130 @@ def test_model_from_results(path, results, thresh_nms=1, use_real=True):
     return aveP, avePred, all_tests, all_scores, all_preds, results
 
 
-def nms(windows, proba, thresh, overlap_rate):
-    pick = []
-    boxes = windows
-    # grab the coordinates of the bounding boxes
-    x1 = boxes[:,0]
-    y1 = boxes[:,1]
-    x2 = boxes[:,2]
-    y2 = boxes[:,3]
 
-    # compute the area of the bounding boxes and sort the bounding
-    # boxes by the bottom-right y-coordinate of the bounding box
-    area = (x2 - x1 + 1) * (y2 - y1 + 1)
-    # proba = proba[np.where(proba < thresh)]
-    idxs = np.argsort(proba)
-    t = proba[idxs]
-    idxs = np.delete(idxs, np.where(t < thresh)[0])
-    # keep looping while some indexes still remain in the indexes
-    # list
-    while len(idxs) > 0:
-        # grab the last index in the indexes list and add the
-        # index value to the list of picked indexes
-        last = len(idxs) - 1
-        i = idxs[last]
-        pick.append(i)
+def nms(dets, sc, Nt=0.3, sigma=0.5, thresh=0.001, method=2):
+    """
+    py_cpu_softnms
+    :param dets:   boexs 坐标矩阵 format [y1, x1, y2, x2]
+    :param sc:     每个 boxes 对应的分数
+    :param Nt:     iou 交叠门限
+    :param sigma:  使用 gaussian 函数的方差
+    :param thresh: 最后的分数门限
+    :param method: 使用的方法
+    :return:       留下的 boxes 的 index
+    """
+
+    # indexes concatenate boxes with the last column
+    N = dets.shape[0]
+    indexes = np.array([np.arange(N)])
+    dets = np.concatenate((dets, indexes.T), axis=1)
+
+    # the order of boxes coordinate is [y1,x1,y2,x2]
+    y1 = dets[:, 0]
+    x1 = dets[:, 1]
+    y2 = dets[:, 2]
+    x2 = dets[:, 3]
+    scores = sc
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+
+    for i in range(N):
+        # intermediate parameters for later parameters exchange
+        tBD = dets[i, :].copy()
+        tscore = scores[i].copy()
+        tarea = areas[i].copy()
+        pos = i + 1
+
+        #
+        if i != N-1:
+            maxscore = np.max(scores[pos:], axis=0)
+            maxpos = np.argmax(scores[pos:], axis=0)
+        else:
+            maxscore = scores[-1]
+            maxpos = 0
+        if tscore < maxscore:
+            dets[i, :] = dets[maxpos + i + 1, :]
+            dets[maxpos + i + 1, :] = tBD
+            tBD = dets[i, :]
+
+            scores[i] = scores[maxpos + i + 1]
+            scores[maxpos + i + 1] = tscore
+            tscore = scores[i]
+
+            areas[i] = areas[maxpos + i + 1]
+            areas[maxpos + i + 1] = tarea
+            tarea = areas[i]
+
+        # IoU calculate
+        xx1 = np.maximum(dets[i, 1], dets[pos:, 1])
+        yy1 = np.maximum(dets[i, 0], dets[pos:, 0])
+        xx2 = np.minimum(dets[i, 3], dets[pos:, 3])
+        yy2 = np.minimum(dets[i, 2], dets[pos:, 2])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[pos:] - inter)
+
+        # Three methods: 1.linear 2.gaussian 3.original NMS
+        if method == 1:  # linear
+            weight = np.ones(ovr.shape)
+            weight[ovr > Nt] = weight[ovr > Nt] - ovr[ovr > Nt]
+        elif method == 2:  # gaussian
+            weight = np.exp(-(ovr * ovr) / sigma)
+        else:  # original NMS
+            weight = np.ones(ovr.shape)
+            weight[ovr > Nt] = 0
+
+        scores[pos:] = weight * scores[pos:]
+
+    # select the boxes and keep the corresponding indexes
+    inds = dets[:, 4][scores > thresh]
+    keep = inds.astype(int)
+    print(keep)
+
+    return keep
+
+# def nms(windows, proba, thresh, overlap_rate):
+#     pick = []
+#     boxes = windows
+#     # grab the coordinates of the bounding boxes
+#     x1 = boxes[:,0]
+#     y1 = boxes[:,1]
+#     x2 = boxes[:,2]
+#     y2 = boxes[:,3]
+
+#     # compute the area of the bounding boxes and sort the bounding
+#     # boxes by the bottom-right y-coordinate of the bounding box
+#     area = (x2 - x1 + 1) * (y2 - y1 + 1)
+#     # proba = proba[np.where(proba < thresh)]
+#     idxs = np.argsort(proba)
+#     t = proba[idxs]
+#     idxs = np.delete(idxs, np.where(t < thresh)[0])
+#     # keep looping while some indexes still remain in the indexes
+#     # list
+#     while len(idxs) > 0:
+#         # grab the last index in the indexes list and add the
+#         # index value to the list of picked indexes
+#         last = len(idxs) - 1
+#         i = idxs[last]
+#         pick.append(i)
  
-        # find the largest (x, y) coordinates for the start of
-        # the bounding box and the smallest (x, y) coordinates
-        # for the end of the bounding box
-        xx1 = np.maximum(x1[i], x1[idxs[:last]])
-        yy1 = np.maximum(y1[i], y1[idxs[:last]])
-        xx2 = np.minimum(x2[i], x2[idxs[:last]])
-        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+#         # find the largest (x, y) coordinates for the start of
+#         # the bounding box and the smallest (x, y) coordinates
+#         # for the end of the bounding box
+#         xx1 = np.maximum(x1[i], x1[idxs[:last]])
+#         yy1 = np.maximum(y1[i], y1[idxs[:last]])
+#         xx2 = np.minimum(x2[i], x2[idxs[:last]])
+#         yy2 = np.minimum(y2[i], y2[idxs[:last]])
  
-        # compute the width and height of the bounding box
-        w = np.maximum(0, xx2 - xx1 + 1)
-        h = np.maximum(0, yy2 - yy1 + 1)
+#         # compute the width and height of the bounding box
+#         w = np.maximum(0, xx2 - xx1 + 1)
+#         h = np.maximum(0, yy2 - yy1 + 1)
  
-        # compute the ratio of overlap
-        overlap = (w * h) / area[idxs[:last]]
+#         # compute the ratio of overlap
+#         overlap = (w * h) / area[idxs[:last]]
  
-        # delete all indexes from the index list that have
-        idxs = np.delete(idxs, np.concatenate(([last], np.where(overlap > overlap_rate)[0])))
+#         # delete all indexes from the index list that have
+#         idxs = np.delete(idxs, np.concatenate(([last], np.where(overlap > overlap_rate)[0])))
  
-    return pick
+#     return pick
